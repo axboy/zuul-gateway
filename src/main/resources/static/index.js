f8e85bc46d7e445ef200331ed5ebc05f159d46d3
zcw <zcw1994@live.com>
2017-08-09 01:01:35 +0800

    format & update

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -17,12 +17,69 @@ let loadData = (data) => {
             size: 20
         },
         columns: [[
-            {field: 'apiName', title: 'Api名'},
-            {field: 'enabled', title: '是否可用'},
-            {field: 'memo', title: '备注'},
-            {field: 'path', title: '路径'},
-            {field: 'url', title: 'url'},
-            {field: 'stripPrefix', title: '去除前缀'}
+            {
+                field: 'apiName',
+                title: 'Api名',
+                width:'10%'
+            },
+            {
+                field: 'path',
+                title: '路径',
+                width:'10%'
+            },
+            {
+                field: 'url',
+                title: 'url',
+                width:'20%'
+            },
+            {
+                field: 'stripPrefix',
+                title: '去除前缀',
+                width: '10%',
+                align: 'center',
+                editor: {
+                    type: 'checkbox',
+                    options: {
+                        on: true,
+                        off: false
+                    }
+                }
+            },
+            {
+                field: 'enabled',
+                title: '是否可用',
+                width: '10%',
+                align: 'center',
+                editor: {
+                    type: 'checkbox',
+                    options: {
+                        on: true,
+                        off: false
+                    }
+                }
+            },
+            {
+                field: 'memo',
+                title: '备注',
+                width:'20%'
+            },
+            {
+                field:'action',
+                title:'操作',
+                width:"20%",
+                align:'center',
+                formatter:(value,row,index)=>{
+                    if (row.editing){
+                        var s = '<a href="#" onclick="saverow(this)">Save</a> ';
+                        var c = '<a href="#" onclick="cancelrow(this)">Cancel</a>';
+                        return s+c;
+                    } else {
+                        var e = '<a href="#" onclick="editrow(this)">Edit</a> ';
+                        var d = '<a href="#" onclick="deleterow(this)">Delete</a>';
+                        return e+d;
+                    }
+                }
+            }
         ]],
         data: data
     });
