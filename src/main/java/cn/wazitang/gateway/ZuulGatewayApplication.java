ddc1978f97dbf8d2db0b083e24490b97e07aa7b5
zcw <zcw1994@live.com>
2017-08-09 23:36:29 +0800

    update index

--- src/main/java/cn/wazitang/gateway/ZuulGatewayApplication.java
+++ src/main/java/cn/wazitang/gateway/ZuulGatewayApplication.java
@@ -3,10 +3,10 @@ package cn.wazitang.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulGatewayApplication {
@@ -17,6 +17,6 @@ public class ZuulGatewayApplication {
 
     @RequestMapping(value = {"/"})
     public String index() {
-        return "gateway: success";
+        return "redirect:/index.html";
     }
 }
