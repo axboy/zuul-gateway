68c7e5577d7dbabdbfcd1cc7275f65e9b3ebd6e5
曾超伟 <zcw1994@live.com>
2017-08-21 19:21:45 +0800

    test remote debug

--- src/main/docker/runboot.sh
+++ src/main/docker/runboot.sh
@@ -1,1 +1,2 @@
-java -Djava.security.egd=file:/dev/./urandom -jar /app/app.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /app/app.jar \
+    -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8088
