40d7bcdd4cc71a056619084a2779a9fbaaa70176
曾超伟 <zcw1994@live.com>
2017-08-21 19:36:04 +0800

    test remote debug

--- src/main/docker/runboot.sh
+++ src/main/docker/runboot.sh
@@ -1,2 +1,4 @@
 java -Djava.security.egd=file:/dev/./urandom -jar /app/app.jar \
-    -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8088
+    -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10022
+
+#    -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8088
